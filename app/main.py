62385f452157a113b67b75001a12dff318c086b0
Limjaeyun <wodbsdot@khu.ac.kr>
2026-04-06 18:44:43 +0900

    update api title to v2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
 
-app = FastAPI(title="Essay PDF Management MLOps Pipeline", description="AI powered PDF splitting service using OpenCV and Tesseract-OCR")
+app = FastAPI(title="Essay PDF Management MLOps Pipeline (v2 업데이트 완료!)", description="AI powered PDF splitting service using OpenCV and Tesseract-OCR")
 
 app.include_router(router, prefix="/api/v1")
 
